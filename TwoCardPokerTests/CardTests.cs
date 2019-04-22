743bb2b1ed398263d886faf6f693b932fad8cc43
Ian <fairbrother.iang@yahoo.co.nz>
2019-04-22 19:47:25 +1200

    corrections to card tests

--- TwoCardPokerTests/CardTests.cs
+++ TwoCardPokerTests/CardTests.cs
@@ -6,21 +6,10 @@ namespace TwoCardPokerTests
     public class CardTests
     {
         [Fact]
-        public void CompareTo_InputCardIsHigherValue_Returns1()
+        public void CompareTo_InputCardIsHigherValue_ReturnsNeg1()
         {
             var losingCard = new Card(CardTypes.Suit.Hearts, CardTypes.Value.Queen);
             var winningCard = new Card(CardTypes.Suit.Hearts, CardTypes.Value.King);
-
-            var result = losingCard.CompareTo(winningCard);
-
-            Assert.Equal(1, result);
-        }
-
-        [Fact]
-        public void CompareTo_InputCardIsLowerValue_ReturnsNeg1()
-        {
-            var losingCard = new Card(CardTypes.Suit.Hearts, CardTypes.Value.King);
-            var winningCard = new Card(CardTypes.Suit.Hearts, CardTypes.Value.Queen);
 
             var result = losingCard.CompareTo(winningCard);
 
@@ -28,14 +17,25 @@ namespace TwoCardPokerTests
         }
 
         [Fact]
-        public void CompareTo_InputCardIsLowerValueButHigherSuit_Returns1()
+        public void CompareTo_InputCardIsLowerValue_Returns1()
+        {
+            var losingCard = new Card(CardTypes.Suit.Hearts, CardTypes.Value.King);
+            var winningCard = new Card(CardTypes.Suit.Hearts, CardTypes.Value.Queen);
+
+            var result = losingCard.CompareTo(winningCard);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void CompareTo_InputCardIsLowerValueButHigherSuit_ReturnsNeg1()
         {
             var losingCard = new Card(CardTypes.Suit.Hearts, CardTypes.Value.King);
             var winningCard = new Card(CardTypes.Suit.Clubs, CardTypes.Value.Queen);
 
             var result = losingCard.CompareTo(winningCard);
 
-            Assert.Equal(1, result);
+            Assert.Equal(-1, result);
         }
     }
 }
