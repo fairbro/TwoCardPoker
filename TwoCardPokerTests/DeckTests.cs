8a76393e78eb66654ecb537a962afccc3d84f800
Ian <fairbrother.iang@yahoo.co.nz>
2019-04-22 13:55:50 +1200

    added GetCard to Deck

---
+++ TwoCardPokerTests/DeckTests.cs
@@ -0,0 +1,30 @@
+using TwoCardPoker;
+using Xunit;
+
+namespace TwoCardPokerTests
+{
+    public class DeckTests
+    {
+        [Fact]
+        public void GetCard_ReturnsFirstCard()
+        {
+            var deck = new Deck();
+
+            var firstCard = deck.GetCard(0);
+
+            Assert.Equal(CardTypes.Suit.Diamonds, firstCard.Suit);
+            Assert.Equal(CardTypes.Value.Two, firstCard.Value);
+        }
+
+        [Fact]
+        public void GetCard_ReturnsLastCard()
+        {
+            var deck = new Deck();
+
+            var lastCard = deck.GetCard(51);
+
+            Assert.Equal(CardTypes.Suit.Spades, lastCard.Suit);
+            Assert.Equal(CardTypes.Value.Ace, lastCard.Value);
+        }
+    }
+}
