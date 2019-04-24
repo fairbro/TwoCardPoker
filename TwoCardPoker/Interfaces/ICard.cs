8e6cda0de2bd6bfe1d5096382a1385864245d48a
Ian <fairbrother.iang@yahoo.co.nz>
2019-04-25 11:25:14 +1200

    moving ICard back to TwoCardPoker project

---
+++ TwoCardPoker/Interfaces/ICard.cs
@@ -0,0 +1,11 @@
+﻿using static Common.CardTypes;
+
+namespace TwoCArdPoker.Interfaces
+{
+    public interface ICard
+    {
+        Suit Suit { get; }
+        Value Value { get; }
+        int CompareTo(ICard card);
+    }
+}
